aad53ef4c94f66d5edbb6b835446951059c8de20
Wang Zhao <081101wangzhao@gmail.com>
2023-09-04 11:20:58 +0800

    bug fix: menu bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
 
     return (
         <header ref={headerRef} className="w-full h-[80px] leading-[80px] flex items-center">
-            <div className="container">
+            <div className="container  overflow-y-clip">
                 <div className="flex items-center justify-between">
                     {/* {  ======================= logo =======================  } */}
                     <div className="flex items-center gap-[10px]">
@@ -72,8 +72,8 @@ const Header = () => {
                                 <button className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300">
                                     <i class="ri-send-plane-line"></i>Let's Talk
                                 </button>
-                            </a>
-                            <span onClick={toggleMenu} className="text-2xl text-smallTextColor md:hidden cursor-pointer"><i class="ri-menu-line"></i></span> */}
+                            </a> */}
+                            <span onClick={toggleMenu} className="text-2xl text-smallTextColor md:hidden cursor-pointer"><i class="ri-menu-line"></i></span>
                         </div>
                     {/* {  ======================= menu right =======================  } */}
                     </div>
